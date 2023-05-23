ba1098c154eb17b45e70b67e45060a3e11028b56
saiminh <saiminh@gmail.com>
2023-05-23 14:00:13 +0200

    align admin css

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '4de7247c8baebcc66a4f');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '3fd748627c468170fecd');
